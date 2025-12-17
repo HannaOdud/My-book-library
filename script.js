2b88f3e4ac97464973e3fc29a8d2c3b9b32d2823
HannaOdud <annodud@gmail.com>
2025-12-17 22:56:01 +0000

    table refresh

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let library = [];
 function setup(){  
     const form = document.getElementById("book_form");
     const titleDom = document.getElementById("title");
@@ -10,8 +11,30 @@ function setup(){
         const author = authorDom.value;
         const pages = pagesDom.value;
         const read = readDom.value;
-        console.log(title+" "+author+" "+pages+" "+read+" Bars!");
+        const book = {
+            title: title,
+            author: author,
+            pages: pages,
+            read: read
+        }
+        library.push(book);
+        displayBooks();
     })   
+}
+function displayBooks(){
+    const tbody = document.getElementById("tbody");
+    tbody.innerHTML=""; //clear table body
+    library.forEach((book) =>{
+        const row = tbody.insertRow();
+        const titleCell = row.insertCell(0);
+        titleCell.textContent = book.title;
+        const authorCell = row.insertCell(1);
+        authorCell.textContent = book.author;
+        const pagesCell = row.insertCell(2);
+        pagesCell.textContent = book.pages;
+        const readCell = row.insertCell(3);
+        readCell.textContent = book.read;
+    });
 }
 
 
