d9211f87dc27cc627b694cb553923a89ffdf48fc
HannaOdud <annodud@gmail.com>
2025-12-17 22:19:39 +0000

    book form submition

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function setup(){
-    console.log("Hello!")
+function setup(){  
+    const form = document.getElementById("book_form");
+    const titleDom = document.getElementById("title");
+    const authorDom = document.getElementById("author");
+    const pagesDom = document.getElementById("pages");
+    const readDom = document.getElementById("checkbox");
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        const title = titleDom.value;
+        const author = authorDom.value;
+        const pages = pagesDom.value;
+        const read = readDom.value;
+        console.log(title+" "+author+" "+pages+" "+read+" Bars!");
+    })   
 }
+
 
 
 window.onload = setup;
